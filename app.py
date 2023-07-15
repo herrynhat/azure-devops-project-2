373df7bc21a47f941058eed5497634dafc1d66d9
herrynhat <herrynhat@gmail.com>
2023-07-15 06:08:00 +0000

    update message

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Hom Hello Viet Nam</h3>"
+    html = "<h3>Sklearn Prediction Hom Hello </h3>"
     return html.format(format)
 
 # TO DO: Log out the prediction value
